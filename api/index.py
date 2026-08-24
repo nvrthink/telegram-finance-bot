b75c80bf67cfacd4ecee7bf3c30809be997e92c3
Yeremia Octavianus <91207398+nvrthink@users.noreply.github.com>
2026-08-24 20:55:59 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -103,7 +103,7 @@ def parse_with_gemini_text(user_text: str) -> list:
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3-flash",
             contents=prompt,
         )
         text_resp = response.text.strip()
